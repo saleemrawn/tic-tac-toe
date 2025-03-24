877603f18dd2434f2b5a3d04f5a4d5ee9f9f6b67
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-03-24 11:11:08 +0000

    Add factory function for game board

---
+++ script.js
@@ -0,0 +1,38 @@
+const gameBoard = (function () {
+  const board = ["", "", "", "", "", "", "", "", ""];
+  let turnCount = 0;
+  let isWinner = false;
+
+  const getBoard = () => board;
+  const addMarkToBoard = (position, mark) => board.splice(position, 1, mark);
+  const increaseTurnCount = () => turnCount++;
+  const checkWinner = (player) => {
+    if (
+      (board[0] === "X" && board[1] === "X" && board[2] === "X") ||
+      (board[3] === "X" && board[4] === "X" && board[5] === "X") ||
+      (board[6] === "X" && board[7] === "X" && board[8] === "X") ||
+      (board[0] === "X" && board[3] === "X" && board[6] === "X") ||
+      (board[1] === "X" && board[4] === "X" && board[7] === "X") ||
+      (board[2] === "X" && board[5] === "X" && board[8] === "X") ||
+      (board[0] === "X" && board[4] === "X" && board[8] === "X") ||
+      (board[2] === "X" && board[4] === "X" && board[6] === "X") ||
+      (board[0] === "O" && board[1] === "O" && board[2] === "O") ||
+      (board[3] === "O" && board[4] === "O" && board[5] === "O") ||
+      (board[6] === "O" && board[7] === "O" && board[8] === "O") ||
+      (board[0] === "O" && board[3] === "O" && board[6] === "O") ||
+      (board[1] === "O" && board[4] === "O" && board[7] === "O") ||
+      (board[2] === "O" && board[5] === "O" && board[8] === "O") ||
+      (board[0] === "O" && board[4] === "O" && board[8] === "O") ||
+      (board[2] === "O" && board[4] === "O" && board[6] === "O")
+    ) {
+      isWinner = true;
+      alert(`${player} wins! Game over!`);
+    }
+  };
+
+  const checkDraw = () => {
+    if (turnCount === 9 && isWinner === false) alert("Draw!");
+  };
+
+  return { addMarkToBoard, increaseTurnCount, getBoard, checkWinner, checkDraw };
+})();
